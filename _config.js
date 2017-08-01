3bd9257fe0c35de803d3118e0e1051de56d941bf
Olaf Veerman <olaf.veerman@gmail.com>
2017-08-01 08:59:05 -0400

    Update prod config

--- _config.js
+++ _config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  baseurl: 'http://localhost:3000',
+  baseurl: 'http://ruralaccess.info',
 
-  title: 'Rural Accessibility Map Documentation',
+  title: 'Documentation',
   description: "",
   metaDescription: "Documentation for RAM",
   sectionsMeta: {
-    's0-introduction': 'Introduction',
-    's1-about': 'About'
+    's0-introduction': 'Introduction'
   }
 };
