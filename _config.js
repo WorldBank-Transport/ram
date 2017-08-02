86af66854b0f7707d6e50ba992eb1c32c18b80e5
Daniel da Silva <daniel@developmentseed.org>
2017-08-02 11:02:52 -0400

    Fix build system for docs

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ module.exports = {
   baseurl: 'http://ruralaccess.info',
 
   title: 'Documentation',
-  description: "",
-  metaDescription: "Documentation for RAM",
+  description: '',
+  metaDescription: 'Documentation for RAM',
   sectionsMeta: {
     's0-introduction': 'Introduction'
   }
